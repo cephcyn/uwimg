1d035304a792b8580c1ede84df4c47bdd3e92c54
Joyce Zhou <jyzhou15@cs.washington.edu>
2021-01-21 17:18:49 -0800

    completed 1.3:

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -28,8 +28,18 @@ image nn_resize(image im, int w, int h)
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // 1.3 TODO
-    return 0;
+    // 1.3 TODONE
+    float v1 = get_pixel(im, (int) floorf(x), (int) floorf(y), c);
+    float v2 = get_pixel(im, (int) ceilf(x), (int) floorf(y), c);
+    float v3 = get_pixel(im, (int) floorf(x), (int) ceilf(y), c);
+    float v4 = get_pixel(im, (int) ceilf(x), (int) ceilf(y), c);
+    float d1 = x - floorf(x);
+    float d2 = ceilf(x) - x;
+    float d3 = y - floorf(y);
+    float d4 = ceilf(y) - y;
+    float q1 = v1*d2 + v2*d1;
+    float q2 = v3*d2 + v4*d1;
+    return q1*d4 + q2*d3;
 }
 
 image bilinear_resize(image im, int w, int h)
