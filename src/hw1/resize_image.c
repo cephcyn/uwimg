9518f0c2e36980b4d9a7ba99a43f4918b9b1d0cb
Joyce Zhou <jyzhou15@cs.washington.edu>
2021-01-21 17:10:39 -0800

    finished 1.2 nn_resize

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -11,8 +11,19 @@ float nn_interpolate(image im, float x, float y, int c)
 
 image nn_resize(image im, int w, int h)
 {
-    // 1.2 TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    // 1.2 TODONE Fill in (also fix that first line)
+    image nim = make_image(w,h,im.c);
+    float rw = ((float) im.w) / w;
+    float rh = ((float) im.h) / h;
+    for (int y = 0; y < nim.h; y++) {
+        for (int x = 0; x < nim.w; x++) {
+            for (int c = 0; c < nim.c; c++) {
+                float p = nn_interpolate(im, (x+0.5)*rw-0.5, (y+0.5)*rh-0.5, c);
+                set_pixel(nim, x, y, c, p);
+            }
+        }
+    }
+    return nim;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
