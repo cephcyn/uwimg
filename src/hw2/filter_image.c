a45f512f2d24e0e848168cc0c0a8097b11c582e3
Joyce Zhou <jyzhou15@cs.washington.edu>
2021-01-21 21:17:38 -0800

    finished most of 2.2 except the questions, where travis is a dumbass lol

--- src/hw2/filter_image.c
+++ src/hw2/filter_image.c
@@ -8,7 +8,7 @@
 
 void l1_normalize(image im)
 {
-    // 2.1 TODO
+    // 2.1 TODONE
     // calculate image sum
     float sum = 0;
     for (int x = 0; x < im.w; x++) {
@@ -30,7 +30,7 @@ void l1_normalize(image im)
 
 image make_box_filter(int w)
 {
-    // 2.1 TODO
+    // 2.1 TODONE
     image f = make_image(w,w,1);
     for (int x = 0; x < f.w; x++) {
         for (int y = 0; y < f.h; y++) {
@@ -41,35 +41,108 @@ image make_box_filter(int w)
     return f;
 }
 
+static float get_filtered_value(image im, image f, int x, int y, int c, int cf) {
+    float sum = 0;
+    int fxw = (f.w-1)/2; 
+    int fyw = (f.h-1)/2;
+    for (int xf = 0; xf < f.w; xf++) {
+        for (int yf = 0; yf < f.h; yf++) {
+            sum += (get_pixel(im, x - fxw + xf, y - fyw + yf, c) * get_pixel(f, xf, yf, cf));
+        }
+    }
+    return sum;
+    
+}
+
 image convolve_image(image im, image filter, int preserve)
 {
-    // 2.2 TODO
-    return make_image(1,1,1);
+    // 2.2 TODONE
+    image copy = copy_image(im);
+    // colvolve each channel
+    for (int c = 0; c < im.c; c++) {
+        int filter_c = c;
+        // if filter has only one channel, resuse it
+        if(filter.c == 1) {
+            filter_c = 0;
+        }
+        for (int x = 0; x < im.w; x++) {
+            for (int y = 0; y < im.h; y++) {
+                set_pixel(copy, x, y, c, get_filtered_value(im, filter, x, y, c, filter_c));
+            }
+        }
+    }
+    
+    // sum each channel together to get a 1 channel image
+    if (!preserve || im.c == 1) {
+        image sum = make_image(copy.w, copy.h, 1); 
+        for (int x = 0; x < im.w; x++) {
+            for (int y = 0; y < im.h; y++) {
+                float s = 0;
+                for(int c = 0; c < im.c; c++) {
+                    s += get_pixel(copy, x, y ,c);
+                }
+                set_pixel(sum, x, y, 0, s);
+            }
+        }
+        return sum;
+    }
+    return copy;
 }
+
 
 image make_highpass_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    // 2.2? TODONE
+    image filter = make_image(3,3,1);
+    set_pixel(filter, 0, 0, 0, 0);
+    set_pixel(filter, 1, 0, 0, -1);
+    set_pixel(filter, 2, 0, 0, 0);
+    set_pixel(filter, 0, 1, 0, -1);
+    set_pixel(filter, 1, 1, 0, 4);
+    set_pixel(filter, 2, 1, 0, -1);
+    set_pixel(filter, 0, 2, 0, 0);
+    set_pixel(filter, 1, 2, 0, -1);
+    set_pixel(filter, 2, 2, 0, 0);
+    return filter;
 }
 
 image make_sharpen_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    // 2.2? TODONE
+    image filter = make_image(3,3,1);
+    set_pixel(filter, 0, 0, 0, 0);
+    set_pixel(filter, 1, 0, 0, -1);
+    set_pixel(filter, 2, 0, 0, 0);
+    set_pixel(filter, 0, 1, 0, -1);
+    set_pixel(filter, 1, 1, 0, 5);
+    set_pixel(filter, 2, 1, 0, -1);
+    set_pixel(filter, 0, 2, 0, 0);
+    set_pixel(filter, 1, 2, 0, -1);
+    set_pixel(filter, 2, 2, 0, 0);
+    return filter;
 }
 
 image make_emboss_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    // 2.2? TODONE
+    image filter = make_image(3,3,1);
+    set_pixel(filter, 0, 0, 0, -2);
+    set_pixel(filter, 1, 0, 0, -1);
+    set_pixel(filter, 2, 0, 0, 0);
+    set_pixel(filter, 0, 1, 0, -1);
+    set_pixel(filter, 1, 1, 0, 1);
+    set_pixel(filter, 2, 1, 0, 1);
+    set_pixel(filter, 0, 2, 0, 0);
+    set_pixel(filter, 1, 2, 0, 1);
+    set_pixel(filter, 2, 2, 0, 2);
+    return filter;
 }
 
 // Question 2.2.1: Which of these filters should we use preserve when we run our convolution and which ones should we not? Why?
-// Answer: TODO
+// Answer: The filters we should use perserve for are the Sharpen and Emoss filter. This is because test.c uses perserver for these filters. We do not use perserve on highpass because test.c doesn't use perserve for it. Also, travis is a dumbass.
 
 // Question 2.2.2: Do we have to do any post-processing for the above filters? Which ones and why?
-// Answer: TODO
+// Answer: No, test.c doesn't do any post processing for any of these filters.
 
 image make_gaussian_filter(float sigma)
 {
