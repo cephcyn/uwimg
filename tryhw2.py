4ac93bad826b180643a2658910e101276c92d874
Joyce Zhou <jyzhou15@cs.washington.edu>
2021-01-27 20:19:53 -0800

    implemented really awful looking ronbledore calculation hahaha

--- tryhw2.py
+++ tryhw2.py
@@ -26,3 +26,17 @@ reconstruct = lfreq + hfreq
 save_image(lfreq, "low-frequency")
 save_image(hfreq, "high-frequency")
 save_image(reconstruct, "reconstruct")
+
+# Ronbledore time! :D
+from uwimg import *
+im_a = load_image("data/dumbledore.png")
+im_b = load_image("data/ron.png")
+f_a = make_gaussian_filter(2)
+lfreq_a = convolve_image(im_a, f_a, 1)
+f_b = make_gaussian_filter(2)
+lfreq_b = convolve_image(im_b, f_b, 1)
+hfreq_b = im_b - lfreq_b
+combined = lfreq_a + hfreq_b
+save_image(combined, "ronbledore")
+
+
